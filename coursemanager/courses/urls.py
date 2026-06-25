65d4eb579f8ae8560da6515957307f4d009cbcd1
Kavin070 <kavingouthamh@gmail.com>
2026-06-25 16:27:22 +0530

    Completed Hands-On 3 Part 1 - DRF APIView

--- coursemanager/courses/urls.py
+++ coursemanager/courses/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import hello_view
+from .views import hello_view, CourseListView, CourseDetailView
 
 urlpatterns = [
     path('hello/', hello_view),
+
+    path('courses/', CourseListView.as_view(), name='course-list'),
+
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
 ]
