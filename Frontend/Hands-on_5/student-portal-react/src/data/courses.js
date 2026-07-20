0d3919f1b6c1e61adf60484620821fda67d8e24b
Kavin070 <kavingouthamh@gmail.com>
2026-07-20 21:12:33 +0530

    Completed Frontend Hands-on 5

---
+++ Frontend/Hands-on_5/student-portal-react/src/data/courses.js
@@ -0,0 +1,39 @@
+const courses = [
+  {
+    id: 1,
+    name: "React Fundamentals",
+    code: "CS301",
+    credits: 4,
+    grade: "A"
+  },
+  {
+    id: 2,
+    name: "Python Programming",
+    code: "CS302",
+    credits: 3,
+    grade: "A+"
+  },
+  {
+    id: 3,
+    name: "Java Programming",
+    code: "CS303",
+    credits: 4,
+    grade: "B+"
+  },
+  {
+    id: 4,
+    name: "Machine Learning",
+    code: "CS304",
+    credits: 5,
+    grade: "A"
+  },
+  {
+    id: 5,
+    name: "Cloud Computing",
+    code: "CS305",
+    credits: 3,
+    grade: "A+"
+  }
+];
+
+export default courses;
